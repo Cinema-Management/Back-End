45d8dc67b290a598367d23dbddabf7906a3144cc
BuiNgocTung <buingoctung1990@gmail.com>
2024-09-21 15:12:58 +0700

    update Controller: movieGenre

--- controllers/movieGenreController.js
+++ controllers/movieGenreController.js
@@ -5,6 +5,7 @@ const movieGenreController = {
     try {
       const { name } = req.body;
 
+      // Kiểm tra xem thể loại đã tồn tại chưa
       const existingGenre = await MovieGenre.findOne({ name });
       if (existingGenre) {
         return res
@@ -12,16 +13,29 @@ const movieGenreController = {
           .send({ message: "Movie genre name already exists" });
       }
 
-      const lastMovieGenre = await MovieGenre.findOne().sort({ code: -1 });
+      // Tìm thể loại phim cuối cùng theo movieGenreId
+      const lastMovieGenre = await MovieGenre.findOne().sort({
+        movieGenreId: -1,
+      });
 
-      let newCode = "MG01";
+      let newCode = "MG01"; // Giá trị mặc định cho mục đầu tiên
       if (lastMovieGenre) {
         const lastCodeNumber = parseInt(lastMovieGenre.code.substring(2));
-        newCode = `MG${String(lastCodeNumber + 1).padStart(2, "0")}`;
+
+        // Tăng số thứ tự
+        const nextCodeNumber = lastCodeNumber + 1;
+
+        // Tạo mã mới với định dạng
+        newCode =
+          nextCodeNumber < 10
+            ? `MG0${nextCodeNumber}` // Nếu số nhỏ hơn 10, thêm 0 vào trước
+            : `MG${nextCodeNumber}`; // Nếu số lớn hơn hoặc bằng 10, giữ nguyên
       }
 
+      // Tạo thể loại phim mới
       const movieGenre = new MovieGenre({ code: newCode, name });
       await movieGenre.save();
+
       return res.status(201).send(movieGenre);
     } catch (error) {
       return res.status(400).send(error);
