f1e74c860d0f24b00576567b5d6a85680b159fe0
BuiNgocTung <buingoctung1990@gmail.com>
2024-09-21 14:51:32 +0700

    add plugins : AutoIncrement, mongoose delete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 const app = express();
 const PORT = process.env.PORT || 8888;
 const mongodb = process.env.MONGODB_URI;
