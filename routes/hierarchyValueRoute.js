4f62e54e35acef9554481defade281b392a50ea6
BuiNgocTung <buingoctung1990@gmail.com>
2024-09-24 00:54:08 +0700

    api cinema: add, select full address, update

--- routes/hierarchyValueRoute.js
+++ routes/hierarchyValueRoute.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 
 router.post("/", hierarchyValueController.add);
 router.get("/", hierarchyValueController.getAll);
-router.get("/:code", hierarchyValueController.getAllByCode);
+router.get("/:code", hierarchyValueController.getFullAddressByCode);
 // router.put("/:code", hierarchyValueController.update);
 
 module.exports = router;
