afa961ecc016c0ffe7ca0de23b4fdcf000ce9b5f
BuiNgocTung <buingoctung1990@gmail.com>
2024-09-28 22:00:43 +0700

    api cinema: delete, restore

--- routes/cinemaRoute.js
+++ routes/cinemaRoute.js
@@ -5,6 +5,7 @@ const router = require("express").Router();
 router.post("/", cinemaController.add);
 router.get("/", cinemaController.getAll);
 router.get("/getAllFullAddress", cinemaController.getAllFullAddress);
-
 router.put("/", cinemaController.update);
+router.patch("/restore/:code", cinemaController.restore);
+router.delete("/:code", cinemaController.delete);
 module.exports = router;
