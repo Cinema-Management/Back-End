898836963d917b52a9f13d00ef3c7ada675c36f4
BuiNgocTung <buingoctung1990@gmail.com>
2024-10-08 18:57:00 +0700

    api promotion v-3

--- models/PromotionDetail .js
+++ models/PromotionDetail .js
@@ -1,16 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+const mongooseDelete = require("mongoose-delete");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 // Schema cho chi tiết khuyến mãi (PromotionDetail)
 const PromotionDetailSchema = new Schema(
   {
-    promotionCode: {
+    code: {
       type: String,
       required: true,
-      ref: "Promotion",
     },
-    itemCode: {
+    //type ===1
+
+    promotionLineCode: {
       type: String,
+      required: true,
+      ref: "PromotionLine",
+    },
+    salesProductCode: {
+      type: String,
+      ref: "Product",
       required: false,
     },
 
@@ -18,17 +26,28 @@ const PromotionDetailSchema = new Schema(
       type: Number,
       required: false, // Số lượng mặt hàng cần mua tối thiểu (dành cho các khuyến mãi kiểu "mua X tặng Y")
     },
+    freeProductCode: {
+      type: String,
+      ref: "Product",
+      required: false,
+    },
+
     freeQuantity: {
       type: Number,
       required: false, // Số lượng tặng (dành cho các khuyến mãi kiểu "mua X tặng Y")
     },
+
     minPurchaseAmount: {
       type: Number,
       required: false, // Giá trị mua tối thiểu để áp dụng khuyến mãi (dành cho các khuyến mãi kiểu "mua Y giảm X%")
     },
+    discountAmount: {
+      type: Number,
+      required: false, // Giảm giá cố định (dành cho các khuyến mãi kiểu "mua Y giảm X%")
+    },
     discountPercentage: {
       type: Number,
-      required: false, // Giảm giá phần trăm (dành cho các khuyến mãi kiểu "mua Y giảm X%")
+      required: false,
     },
     maxDiscountAmount: {
       type: Number,
@@ -36,13 +55,48 @@ const PromotionDetailSchema = new Schema(
     },
 
     type: {
-      type: String,
-      enum: ["discount", "free"],
+      type: Number,
+      enum: [0, 1, 2],
       required: true, // Loại khuyến mãi: "discount" (giảm giá), "free" (tặng hàng)
     },
   },
-  { timestamps: true }
+  { timestamps: true, collection: "promotion_details" }
 );
+
+PromotionDetailSchema.virtual("relevantFields").get(function () {
+  const fields = [];
+  switch (this.type) {
+    case 0:
+      fields.push(
+        "productSalesCode",
+        "minQuantity",
+        "productSalesFree",
+        "freeQuantity"
+      );
+      break;
+    case 1:
+      fields.push("minPurchaseAmount", "discountAmount");
+      break;
+    case 2:
+      fields.push(
+        "minPurchaseAmount",
+        "discountPercentage",
+        "maxDiscountAmount"
+      );
+      break;
+    default:
+      break;
+  }
+  return fields;
+});
+// Add plugins
+PromotionDetailSchema.plugin(AutoIncrement, {
+  inc_field: "promotionDetailId",
+});
+PromotionDetailSchema.plugin(mongooseDelete, {
+  deletedAt: true,
+  overrideMethods: "all",
+});
 
 const PromotionDetail = mongoose.model(
   "PromotionDetail",
