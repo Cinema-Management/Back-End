6be2e696daba54b71dd7a6c66555a3f9a8a60cad
BuiNgocTung <buingoctung1990@gmail.com>
2024-10-10 20:17:59 +0700

    promotion v-3

--- routes/promotionLineRoute.js
+++ routes/promotionLineRoute.js
@@ -2,8 +2,6 @@ const promotionLineController = require("../controllers/promotionLineController"
 const router = require("express").Router();
 
 router.get("/", promotionLineController.getAll);
-router.get("/getPromotionDetailsByDateAndStatus", promotionLineController.getPromotionDetailsByDateAndStatus);
-
 
 router.post("/", promotionLineController.add);
 router.delete("/:code", promotionLineController.delete);
